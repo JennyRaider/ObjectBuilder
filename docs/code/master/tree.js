5b6d77d223bb2e492d394bb87a42b8b02e78d50f
Gary Saunders <gary@7up-laptop.(none)>
2014-01-02 23:11:42 -0500

    Sweet, sweet docs.

---
+++ docs/code/master/tree.js
@@ -0,0 +1,10 @@
+var tree = [["JennyRaider",,"",
+                [["Code","JennyRaider\/Code.html","",
+                [["ClassDefinition","JennyRaider\/Code\/ClassDefinition.html"," < ObjectDefinition",
+                [        ]],["ObjectDefinition","JennyRaider\/Code\/ObjectDefinition.html"," < ObjectNameDefinition",
+                [        ]],["ObjectDefinitionBuilder","JennyRaider\/Code\/ObjectDefinitionBuilder.html","",
+                [        ]],["ObjectNameDefinition","JennyRaider\/Code\/ObjectNameDefinition.html","",
+                [        ]],["ObjectPresenter","JennyRaider\/Code\/ObjectPresenter.html","",
+                [        ]],["VariableDefinition","JennyRaider\/Code\/VariableDefinition.html","",
+                [        ]]        ]]        ]]        ]
+
