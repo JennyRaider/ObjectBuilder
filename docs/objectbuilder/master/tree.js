07c43b411c6bba9220ac3c5eb91cb7976a366414
Gary Saunders <gary@powrit.com>
2014-01-23 02:08:36 -0500

    Updating docs.

---
+++ docs/objectbuilder/master/tree.js
@@ -0,0 +1,11 @@
+var tree = [["JennyRaider",,"",
+                [["ObjectBuilder","JennyRaider\/ObjectBuilder.html","",
+                [["ClassDefinition","JennyRaider\/ObjectBuilder\/ClassDefinition.html"," < ObjectDefinition",
+                [        ]],["ObjectBuilderInterface","JennyRaider\/ObjectBuilder\/ObjectBuilderInterface.html","",
+                [        ]],["ObjectDefinition","JennyRaider\/ObjectBuilder\/ObjectDefinition.html"," < ObjectNameDefinition",
+                [        ]],["ObjectDefinitionBuilder","JennyRaider\/ObjectBuilder\/ObjectDefinitionBuilder.html","",
+                [        ]],["ObjectNameDefinition","JennyRaider\/ObjectBuilder\/ObjectNameDefinition.html","",
+                [        ]],["ObjectPresenter","JennyRaider\/ObjectBuilder\/ObjectPresenter.html","",
+                [        ]],["VariableDefinition","JennyRaider\/ObjectBuilder\/VariableDefinition.html","",
+                [        ]]        ]]        ]]        ]
+
