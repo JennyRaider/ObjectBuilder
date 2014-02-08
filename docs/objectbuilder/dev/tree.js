0ac1063975dfad435cd49017840732e13246f34c
Gary Saunders <gary@powrit.com>
2014-02-08 16:08:35 -0500

    Doc update, works well now!

---
+++ docs/objectbuilder/dev/tree.js
@@ -0,0 +1,10 @@
+var tree = [["JennyRaider",,"",
+                [["ObjectBuilder","JennyRaider\/ObjectBuilder.html","",
+                [["ObjectBuilder","JennyRaider\/ObjectBuilder\/ObjectBuilder.html","",
+                [        ]],["ObjectBuilderInterface","JennyRaider\/ObjectBuilder\/ObjectBuilderInterface.html","",
+                [        ]],["ObjectDefinition","JennyRaider\/ObjectBuilder\/ObjectDefinition.html"," < ObjectNameDefinition",
+                [        ]],["ObjectNameDefinition","JennyRaider\/ObjectBuilder\/ObjectNameDefinition.html","",
+                [        ]],["ObjectPresenter","JennyRaider\/ObjectBuilder\/ObjectPresenter.html","",
+                [        ]],["VariableDefinition","JennyRaider\/ObjectBuilder\/VariableDefinition.html","",
+                [        ]]        ]]        ]]        ]
+
